546cf26456cf1f94dfe6e3694eb407273f4e3203
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-19 11:19:20 +0300

    view function and url for passenger signup and login complete

--- passenger/urls.py
+++ passenger/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns =[
     url(r'^$', views.index, name='index'),
     url(r'passenger/', views.passenger, name= 'passenger'),
+    url(r'^passenger/signup/$', views.passenger_signup, name='passenger_signup'),
+    url(r'^passenger/login/$', views.passenger_login, name='passenger_login'),
 ]   
--- passenger/views.py
+++ passenger/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import SignUpForm, LoginForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
 def passenger(request):
     return render(request, 'passenger/passenger.html')
+
+def passenger_signup(request):
+    form = SignUpForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authentication(username=username, password=raw_password)
+            login(request, user)
+            return redirect('passenger')
+        else:
+            form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
+def passenger_login(request):
+    form = LoginForm()
+    return render(request,'registration/login.html',{"form":form})
