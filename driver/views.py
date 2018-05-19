bea31dd88edbafaef1816c822006cfedce705639
wesleymutwiri <wesleymochiemo@gmail.com>
2018-05-19 12:29:42 +0300

    confirmed signup for driver is working

--- driver/views.py
+++ driver/views.py
@@ -4,24 +4,25 @@ from django.contrib.auth.decorators import login_required
 import datetime as dt 
 from django.contrib.auth.forms import UserCreationForm
 from django.db import transaction
+from django.contrib.auth import login, authenticate
 
 # Create your views here.
 def driver(request):
     return render(request, 'driver/driver.html')
 
 def driver_signup(request):
-    form = SignUpForm
+    # form = SignUpForm
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
-            user = authentication(username=username, password=raw_password)
+            user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('driver')
-        else:
-            form = SignUpForm()
+    else:
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
             
 def driver_login(request):
